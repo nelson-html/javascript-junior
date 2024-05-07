fe1f41fcda375966f157486cb6bc54fac821d62c
nelson_html <martineznelson515@gmail.com>
2024-05-07 10:55:44 -0600

    avanzando en el curso de javascript chunior

---
+++ index.js
@@ -0,0 +1,108 @@
+// aca abajo podemos observar un ejemplo de Scope de varibales globales y locales.
+
+/*
+
+let pedro = "pedro"
+
+if (5 > 1){
+    var juan = "juan"
+
+    document.write(pedro," ", juan);
+}
+
+document.write(" ",juan) */
+//----------------------------------------------------------------------------------
+
+//USANDO PROMPT
+
+/*
+
+let nombre = prompt("Hola prix");
+
+document.write("hola "+ nombre) */
+
+//----------------------------------------------------------------------------------
+
+// OPERADORES DE ASIGNACION
+/*
+let numero = 10;
+let numero1 = 10;
+let numero2= 10;
+let numero3 = 10;
+let numero4 = 10;
+let numero5 = 10;
+let numero5_5 = 10;
+
+numero += 5;
+numero1 -= 5;
+numero2 *= 5;
+numero3 /= 5;
+numero4 %= 6;
+numero5 **= 3;
+numero5_5 = numero5_5 ** 3;
+
+document.write(numero,"\n")
+document.write(numero1,"\n")
+document.write(numero2,"\n")
+document.write(numero3,"\n")
+document.write(numero4,"\n")
+document.write(numero5,"\n")
+document.write(numero5_5,"\n")
+*/
+//----------------------------------------------------------------------------------
+
+
+//OPERADORES ARITMETICOS
+
+/*
+numero1 = 10;
+numero2 = 5;
+numero3 = 5;
+
+numero1--
+numero2++
+resultado = numero1;
+resultado1 = numero2;
+resultado2 = numero3**2;
+document.write(resultado)
+document.write("\n\-------- ",resultado1)
+document.write("\n\-------- ",resultado2)*/
+//----------------------------------------------------------------------------------
+
+// Uso de condicionales
+/*
+let nombre = "ruizs";
+
+if (nombre == "nelson") {
+    document.write("Que pros, NELSON");
+}
+else if (nombre == "ruiz") {
+    document.write("Que giga pros eres ruiz");
+}
+else{
+    document.write("Tu eres otro...")
+}
+*/
+//----------------------------------------------------------------------------------
+
+//arrays en general
+/*
+let ejemplo = ["contenido", 15, "Hola", "XD"];
+
+document.write(ejemplo )
+*/
+/*
+let pc1 = {
+    nombre: "NelsonPcerdo",
+    procesador: "Intel Core i5",
+    ram: "16gb",
+    espacio: "1TB"
+}
+
+document.write(pc1["nombre"]);
+document.write(pc1[1]); //debemos de poner los nombres de los obejetos definidos
+*/
+//----------------------------------------------------------------------------------
+
+
+
