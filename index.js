65bd4113bd2b9183eddcb5e6f4e5ef940c2a98e6
nelson_html <martineznelson515@gmail.com>
2024-05-28 23:13:59 -0600

    pasando al 4 tema, los metodos de cadenas

--- index.js
+++ index.js
@@ -104,5 +104,136 @@ document.write(pc1[1]); //debemos de poner los nombres de los obejetos definidos
 */
 //----------------------------------------------------------------------------------
 
+// bucles
+/*
+let numero = 0;
 
+while (numero < 1000){
+    numero++;
+    document.write(numero + "<br> ")
+    if (numero == 15) {
+        break;
+    }
+}
+*/
 
+//----------------------------------------------------------------------------------
+//uso de continue
+/*
+numero = 50;
+
+for (let i = 10; i > 0; i--) {
+    
+    if (i == 5) {
+        continue;
+    }
+    document.write(i + "<br>");
+    
+}*/
+//----------------------------------------------------------------------------------
+//uso de for in y for of
+/*
+let array1 = ["Maria", "Josefa", "Carla"];
+let array2 = ["Marcos", "Calvino", "Ignacio de Layola", array1];
+
+for (let array in array2){
+    if (array == 3){
+        for (let array of array1){
+            document.write(array + "<br>")
+        }
+    }
+    else{
+        document.write(array2[array] + "<br>")
+    }
+}
+*/
+
+//----------------------------------------------------------------------------------      
+//funcion flecha
+/*
+const saludar = () => {
+   let respuesta = prompt("Hola que tal te va?")
+    if (respuesta == "bien"){
+        alert("Me alegro");
+    }
+    else{
+        alert("una pena")
+    }
+}
+
+saludar();
+saludar();
+saludar();
+saludar();*/
+//----------------------------------------------------------------------------------
+
+//uso de return en funciones
+
+/*
+function saludar(){
+    alert("Hola prix");
+    a = 10;
+    b = 5;
+
+    c = a+b;
+    return c
+    document.write(c)
+}
+
+let respuesta = saludar();
+document.write(respuesta);
+*/
+//----------------------------------------------------------------------------------
+
+//Empenzando a usar POO
+
+class Animal {
+    constructor (especie, edad, color){
+        this.especie = especie;
+        this.edad = edad;
+        this.color = color;
+        this.info = `Soy un ${especie}, tengo la de edad de ${edad} años y un color ${color} <br>`
+    }
+    verInfo(){
+        document.write(this.info);
+    }
+    /*ladrar(){
+        if (this.especie == "Perro"){
+            document.write("WAF WAF" + "<br>")
+        }
+        else{
+            document.write("No puede ladrar porque es un ", this.especie)
+        }
+    }*/
+}
+
+class Perro extends Animal{
+    constructor (especie, edad, color, raza){
+        super(especie, edad, color);
+        this.raza = null;
+    }
+    // static ladrar(){
+    //     document.write("El perro dice WAW WAW WAW XD")
+    // }
+
+    set setRaza(newName){
+        this.raza = newName;
+    }
+    get getRaza(){
+        return this.raza;
+    }
+}
+
+const perro = new Perro("Perro", 10, "Verde", "pitbull");
+const gato = new Animal("Gato", 2, "cafe");
+
+// document.write(perro.info)
+// document.write(gato.info)
+
+// perro.verInfo();
+// gato.verInfo();
+
+perro.setRaza = "Pedro"
+
+document.write(perro.getRaza)
+gato.ladrar();
